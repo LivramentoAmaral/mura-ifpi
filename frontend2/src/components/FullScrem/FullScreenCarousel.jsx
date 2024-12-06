e8d45bddf0154912d2b4626db4a8f013df4d6150
Uenderson-Mendes <uemdersonbebe@gmail.com>
2024-12-05 23:28:22 -0300

    atualizaçao tela de apresentação

--- frontend2/src/components/FullScrem/FullScreenCarousel.jsx
+++ frontend2/src/components/FullScrem/FullScreenCarousel.jsx
@@ -1,67 +1,89 @@
 import React, { useState, useEffect, useRef } from 'react';
 import style from './style.module.css';
-import FullscreenIcon from '@mui/icons-material/Fullscreen'; 
-import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
-import { Link } from 'react-router-dom';  // Importa o ícone FullscreenExit
+import { useNavigate } from 'react-router-dom';  // useNavigate para navegação
 
 const FullScreenCarousel = ({ images }) => {
-  const [isFullScreen, setIsFullScreen] = useState(false);
-  const [currentIndex, setCurrentIndex] = useState(0); // Estado para controlar o índice da imagem atual
-  const carouselRef = useRef(null); // Ref para acessar o contêiner do carrossel
-  const fullscreenButtonRef = useRef(null); // Ref para o botão de tela cheia
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [showMessage, setShowMessage] = useState(false);
+  const carouselRef = useRef(null);
+  const navigate = useNavigate();
 
-  // Função para alternar entre o modo tela cheia
-  const toggleFullScreen = () => {
-    if (!isFullScreen) {
-      if (carouselRef.current.requestFullscreen) {
-        carouselRef.current.requestFullscreen();
-      } else if (carouselRef.current.mozRequestFullScreen) { // Firefox
-        carouselRef.current.mozRequestFullScreen();
-      } else if (carouselRef.current.webkitRequestFullscreen) { // Chrome, Safari
-        carouselRef.current.webkitRequestFullscreen();
-      } else if (carouselRef.current.msRequestFullscreen) { // IE/Edge
-        carouselRef.current.msRequestFullscreen();
-      }
-    } else {
-      document.exitFullscreen(); // Sair do modo tela cheia
+  // Função para entrar em modo tela cheia
+  const enterFullScreen = () => {
+    if (carouselRef.current.requestFullscreen) {
+      carouselRef.current.requestFullscreen().catch(err => {
+        console.error("Error attempting to enable full-screen mode:", err.message);
+      });
+    } else if (carouselRef.current.mozRequestFullScreen) {
+      carouselRef.current.mozRequestFullScreen(); // Firefox
+    } else if (carouselRef.current.webkitRequestFullscreen) {
+      carouselRef.current.webkitRequestFullscreen(); // Chrome, Safari
+    } else if (carouselRef.current.msRequestFullscreen) {
+      carouselRef.current.msRequestFullscreen(); // IE/Edge
     }
-    setIsFullScreen(!isFullScreen); // Alterna o estado do botão
   };
 
-  // Função para passar para a próxima imagem
-  const nextImage = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length); // Vai para a próxima imagem, reinicia quando atingir o fim
+  // Função para sair do modo de tela cheia
+  const exitFullScreen = () => {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen(); // Firefox
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen(); // Chrome, Safari
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen(); // IE/Edge
+    }
   };
 
   // Configurar um intervalo para passar as imagens automaticamente
+  const nextImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
   useEffect(() => {
-    const interval = setInterval(nextImage, 5000); // Passa a imagem a cada 5 segundos
-    return () => clearInterval(interval); // Limpa o intervalo quando o componente for desmontado
+    enterFullScreen(); // Entrar em tela cheia assim que o componente for carregado
+    const interval = setInterval(nextImage, 5000);
+    const messageTimeout = setTimeout(() => {
+      setShowMessage(true);
+    }, 3000); // Mostrar a mensagem após 3 segundos
+
+    // Timeout para ocultar a mensagem após 2 minutos
+    const hideMessageTimeout = setTimeout(() => {
+      setShowMessage(false);
+    }, 120000); // 2 minutos
+
+    return () => {
+      clearInterval(interval);
+      clearTimeout(messageTimeout);
+      clearTimeout(hideMessageTimeout);
+    };
   }, [images.length]);
 
-  // Função para lidar com a tecla 'Esc' para alternar o estado de tela cheia
+  // Função para redirecionar para a página de admin
+  const handleCloseClick = () => {
+    exitFullScreen();
+    navigate('/');
+  };
+
+  // Função para lidar com a tecla 'X'
   const handleKeyPress = (event) => {
-    if (event.key === 'Escape') {
-      if (isFullScreen) {
-        document.exitFullscreen(); // Sair do modo tela cheia
-        setIsFullScreen(false); // Atualiza o estado para refletir que o modo não está mais ativo
-      }
+    if (event.key === 'x' || event.key === 'X') {
+      exitFullScreen();
+      navigate('/');
     }
   };
 
-  // Adicionar o evento de tecla para o 'Esc' ao montar o componente
+  // Adicionar o evento de tecla para o 'X' ao montar o componente
   useEffect(() => {
     window.addEventListener('keydown', handleKeyPress);
     return () => {
       window.removeEventListener('keydown', handleKeyPress);
     };
-  }, [isFullScreen]);
+  }, []);
 
   return (
-    <div
-      ref={carouselRef}
-      className={`${style.fullscreencarousel} ${isFullScreen ? `${style.fullscreen}` : ''}`}
-    >
+    <div ref={carouselRef} className={`${style.fullscreencarousel}`}>
       {images.length > 0 && (
         <div className={style.imagecontainer}>
           <img
@@ -69,24 +91,23 @@ const FullScreenCarousel = ({ images }) => {
             alt={images[currentIndex].alt}
             className={style.carouselimage}
           />
-          {images[currentIndex].text && <div className={style.carouseltext}>{images[currentIndex].text}</div>}
+          {images[currentIndex].text && (
+            <div className={style.carouseltext}>{images[currentIndex].text}</div>
+          )}
         </div>
       )}
       <button
-        ref={fullscreenButtonRef}
-        className={style.fullscreenbutton}
-        onClick={toggleFullScreen}
+        className={style.closeButton}
+        onClick={handleCloseClick}
+        style={{ position: 'absolute', top: '10px', right: '20px' }}
       >
-        {/* Exibe o ícone correspondente */}
-        {isFullScreen ? <FullscreenExitIcon sx={{
-          fontSize: 54,
-          color: 'white'
-        }} /> : <FullscreenIcon sx={{
-          fontSize: 54,
-          color: 'white'
-        }} />}
+        X
       </button>
-      <Link to="/">retornar a página inicial</Link>
+      {showMessage && (
+        <div className={style.exitMessage} style={{ position: 'absolute', top: '10px', right: '60px', color: 'white', backgroundColor: 'rgba(0, 0, 0, 0.7)', padding: '10px', borderRadius: '5px' }}>
+          Para sair, aperte no ➡️
+        </div>
+      )}
     </div>
   );
 };
