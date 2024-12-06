9b1bce0dc017939dec318248dace48fe7ae84fa9
LivramentoAmaral <marcosdoamaral2002@gmail.com>
2024-12-06 14:29:51 -0300

    Atulização no componente FullScreemCarrocel

--- frontend2/src/components/FullScrem/FullScreenCarousel.jsx
+++ frontend2/src/components/FullScrem/FullScreenCarousel.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import style from './style.module.css';
-import { useNavigate } from 'react-router-dom';  // useNavigate para navegação
+import { useNavigate } from 'react-router-dom';
 
 const FullScreenCarousel = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -8,82 +8,84 @@ const FullScreenCarousel = ({ images }) => {
   const carouselRef = useRef(null);
   const navigate = useNavigate();
 
-  // Função para entrar em modo tela cheia
+  const isFullScreenActive = () =>
+    document.fullscreenElement ||
+    document.mozFullScreenElement ||
+    document.webkitFullscreenElement ||
+    document.msFullscreenElement;
+
   const enterFullScreen = () => {
-    if (carouselRef.current.requestFullscreen) {
-      carouselRef.current.requestFullscreen().catch(err => {
-        console.error("Error attempting to enable full-screen mode:", err.message);
-      });
-    } else if (carouselRef.current.mozRequestFullScreen) {
-      carouselRef.current.mozRequestFullScreen(); // Firefox
-    } else if (carouselRef.current.webkitRequestFullscreen) {
-      carouselRef.current.webkitRequestFullscreen(); // Chrome, Safari
-    } else if (carouselRef.current.msRequestFullscreen) {
-      carouselRef.current.msRequestFullscreen(); // IE/Edge
+    if (carouselRef.current) {
+      const requestFullscreen =
+        carouselRef.current.requestFullscreen ||
+        carouselRef.current.mozRequestFullScreen ||
+        carouselRef.current.webkitRequestFullscreen ||
+        carouselRef.current.msRequestFullscreen;
+
+      if (requestFullscreen) {
+        requestFullscreen.call(carouselRef.current).catch((err) => {
+          console.error('Error attempting to enable full-screen mode:', err.message);
+        }, []);
+      }
     }
   };
 
-  // Função para sair do modo de tela cheia
-  const exitFullScreen = () => {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.mozCancelFullScreen) {
-      document.mozCancelFullScreen(); // Firefox
-    } else if (document.webkitExitFullscreen) {
-      document.webkitExitFullscreen(); // Chrome, Safari
-    } else if (document.msExitFullscreen) {
-      document.msExitFullscreen(); // IE/Edge
-    }
-  };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const exitFullScreen = useCallback(() => {
+    if (isFullScreenActive()) {
+      const exitFullscreen =
+        document.exitFullscreen ||
+        document.mozCancelFullScreen ||
+        document.webkitExitFullscreen ||
+        document.msExitFullscreen;
 
-  // Configurar um intervalo para passar as imagens automaticamente
+      if (exitFullscreen) {
+        exitFullscreen.call(document).catch((err) => {
+          console.error('Error attempting to exit full-screen mode:', err.message);
+        });
+      }
+    }
+  });
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const nextImage = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
   useEffect(() => {
-    enterFullScreen(); // Entrar em tela cheia assim que o componente for carregado
+    enterFullScreen();
     const interval = setInterval(nextImage, 5000);
-    const messageTimeout = setTimeout(() => {
-      setShowMessage(true);
-    },0); // Mostrar a mensagem após 3 segundos
-
-    // Timeout para ocultar a mensagem após 2 minutos
-    const hideMessageTimeout = setTimeout(() => {
-      setShowMessage(false);
-    }, 120000); // 2 minutos
+    const messageTimeout = setTimeout(() => setShowMessage(true), 0);
+    const hideMessageTimeout = setTimeout(() => setShowMessage(false), 120000);
 
     return () => {
       clearInterval(interval);
       clearTimeout(messageTimeout);
       clearTimeout(hideMessageTimeout);
     };
-  }, [images.length]);
+  }, [images.length, nextImage]);
 
-  // Função para redirecionar para a página de admin
   const handleCloseClick = () => {
     exitFullScreen();
     navigate('/');
   };
 
-  // Função para lidar com a tecla 'X'
-  const handleKeyPress = (event) => {
+  const handleKeyPress = useCallback((event) => {
     if (event.key === 'x' || event.key === 'X') {
       exitFullScreen();
       navigate('/');
     }
-  };
+  }, [exitFullScreen, navigate]);
 
-  // Adicionar o evento de tecla para o 'X' ao montar o componente
   useEffect(() => {
     window.addEventListener('keydown', handleKeyPress);
     return () => {
       window.removeEventListener('keydown', handleKeyPress);
     };
-  }, []);
+  }, [handleKeyPress]);
 
   return (
-    <div ref={carouselRef} className={`${style.fullscreencarousel}`}>
+    <div ref={carouselRef} className={style.fullscreencarousel}>
       {images.length > 0 && (
         <div className={style.imagecontainer}>
           <img
@@ -91,7 +93,6 @@ const FullScreenCarousel = ({ images }) => {
             alt={images[currentIndex].alt}
             className={style.carouselimage}
           />
-         
         </div>
       )}
       <button
@@ -102,7 +103,18 @@ const FullScreenCarousel = ({ images }) => {
         X
       </button>
       {showMessage && (
-        <div className={style.exitMessage} style={{ position: 'absolute', top: '10px', right: '60px', color: 'white', backgroundColor: 'rgba(0, 0, 0, 0.7)', padding: '10px', borderRadius: '5px' }}>
+        <div
+          className={style.exitMessage}
+          style={{
+            position: 'absolute',
+            top: '10px',
+            right: '60px',
+            color: 'white',
+            backgroundColor: 'rgba(0, 0, 0, 0.7)',
+            padding: '10px',
+            borderRadius: '5px',
+          }}
+        >
           Para sair, aperte no ➡️
         </div>
       )}
